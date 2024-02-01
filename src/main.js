0479e03b9bdaa475f1dd1cf9b8140c8cb55429bb
Vitaliy <131911773+whyzzz20@users.noreply.github.com>
2024-02-01 19:03:31 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,145 +1,187 @@
-//Імпорти бібліотек
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-//налаштування axios
-axios.defaults.baseURL = 'https://pixabay.com/';
+const BASE_URL= "https://pixabay.com/api/";
+const API_KEY = "41911500-2109ce3c8bb16633259977e96";
 
-//Посилання на форму та галерею
+const container = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
-
+const input= document.querySelector('.search-input');
+const startBtn= document.querySelector('.start-btn');
 const loader = document.querySelector('.loader');
-const loadMoreBtn = document.querySelector('.load-more');
+const loadMoreBtn = document.querySelector('.load-btn');
+const preloader = document.querySelector('loader-load');
 
-//Прослуховування "сабміту" форми
-
-let searchQuery = '';
+const hiddenClass=  "is-hidden";
+let inputValue = "";
 let page = 1;
-let lightbox;
-form.addEventListener('submit', onFormSuccess);
+let maxPage = 0;
 
-// Отримання і відмальовка фото
-function onFormSuccess(e) {
-  e.preventDefault();
-  page = 1;
 
-  searchQuery = e.target.elements.searchQuery.value.trim();
+const refreshPage = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
+// const axios = require('axios').default;
+
+
+loader.style.display = 'none';
+form.addEventListener('submit', handleSearch)
+
+function handleSearch(event){
+  event.preventDefault();
   loader.style.display = 'block';
+  inputValue = event.target.elements.query.value;
+  fetchImage(inputValue)
+  .then(data => {
+    loader.style.display = 'none';
+      if(!data.hits.length){
+          iziToast.error({
+              title: 'Error',
+              message:
+                'Sorry, there are no images matching your search query. Please try again!',
+            });
+      } 
+      container.innerHTML = "";
+      page = 1;
+      try {
+       
+        container.innerHTML = createMarkup(data.hits);
+      // const refreshPage = new SimpleLightbox('.gallery a', {
+      //     captionsData: 'alt',
+      //     captionDelay: 250,
+      //   });
+        refreshPage.refresh();
+        scrollBy();
+        // ------------кнопка ще
+        if(data.totalHits > 0){
+          loadMoreBtn.classList.remove(hiddenClass);
+          loadMoreBtn.addEventListener("click", handleLoadMore);
+      }else{
+          loadMoreBtn.classList.add(hiddenClass);
+      }
 
-  searchImage(searchQuery)
-    .then(r => {
-      if (r.hits.length === 0) {
-        gallery.innerHTML = '';
-        loadMoreBtn.style.display = 'none';
-        return iziToast.warning({
-          title: 'Wrong request',
+      } catch (error) {
+        onFetchError(error);
+      }finally {
+        form.reset();
+        if(page === maxPage){
+          loadMoreBtn.classList.add(hiddenClass);
+        iziToast.error({
+          title: 'Error',
           message:
-            'Sorry, there are no images matching your search query. Please try again!',
+            "We're sorry, but you've reached the end of search results.",
         });
+        
+        }
       }
-
-      gallery.innerHTML = makeMarkup(r.hits);
-
-      loader.style.display = 'none';
-
-      loadMoreBtn.style.display = 'block';
-
-      lightbox = new SimpleLightbox('.gallery a');
-
-      if (r.totalHits <= page * 40) {
-        iziToast.success({
-          title: "That's all",
-          message: 'Try another word',
-        });
-        loadMoreBtn.style.display = 'none';
-      }
-    })
-    .catch(error =>
-      iziToast.error({
-        title: 'Error',
-        message: `Oh, we have problem: ${error}`,
+        
+        
       })
-    );
-  e.target.elements.searchQuery.value = '';
+  
 }
 
-//Прослуховування LoadMoreBtn
 
-loadMoreBtn.addEventListener('click', loadMoreClick);
+function fetchImage(name, page = 1){
+  return axios.get(`${BASE_URL}?key=${API_KEY}&q=${name}`,{
+  params : {
+apiKey: API_KEY,
+q: name,
+image_type: 'photo',
+orientation: 'horizontal',
+safesearch: true,
+per_page: 40,
+page,
+  }
+}).then(({data})=>data)};
+// const url = `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
 
-function loadMoreClick() {
-  page += 1;
-  searchImage(searchQuery).then(r => {
-    gallery.insertAdjacentHTML('beforeend', makeMarkup(r.hits));
-    if (r.totalHits <= page * 40) {
-      iziToast.warning({
-        title: 'Try another word',
-        message: "We're sorry, but you've reached the end of search results.",
-      });
+//     return fetch(url).then((resp)=> {
+//         if(!resp.ok){
+//             throw new Error(resp.statusText);
+//         }
+//         return resp.json();
+//     })
+    
 
-      loadMoreBtn.style.display = 'none';
-      return;
-    }
-    lightbox.refresh();
-    // const photoRef = document.querySelector('.gallery__link');
-    const height = document
-      .querySelector('.gallery__link')
-      .getBoundingClientRect();
 
-    window.scrollBy({
-      top: height.height * 2,
-      behavior: 'smooth',
-    });
-  });
-}
-
-//Функція запиту данних по пошуковому слову
-async function searchImage(searchWord) {
-  const response = await axios.get('api/', {
-    params: {
-      key: '41293253-42a55b268bdac57d89d3cc200',
-      q: searchWord,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      per_page: 40,
-      page,
-    },
-  });
-
-  return response.data;
-}
-
-//Створення розмітки з масиву
-
-function makeMarkup(arr) {
+function createMarkup(arr) {
   return arr
     .map(
-      ({
-        webformatURL,
+      ({ webformatURL,
         largeImageURL,
         tags,
         likes,
         views,
         comments,
-        downloads,
-        id,
-      }) => `<a class="gallery__link" href="${largeImageURL}">
-  <div class="gallery-item" id="${id}">
-    <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy">
-    <div class="info">
-      <p class="info-item"><b>Likes</b>${likes}</p>
-      <p class="info-item"><b>Views</b>${views}</p>
-      <p class="info-item"><b>Comments</b>${comments}</p>
-      <p class="info-item"><b>Downloads</b>${downloads}</p>
-    </div>
-  </div>
-</a>`
+        downloads, }) => `
+  <li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${webformatURL}"
+      alt="${tags}"
+    />
+    <p class= "gallery-descr">• Likes: ${likes} • Views: ${views} • Comments: ${comments} •</span> Downloads:${downloads}</p>
+  </a>
+</li>
+  `
     )
     .join('');
+    
+}
+
+async function handleLoadMore(){
+  page += 1;
+ 
+loadMoreBtn.disabled = true;
+  try {
+    const {hits, total} = await fetchImage(inputValue, page);
+
+    maxPage = Math.ceil(total / 40);
+    
+    
+    container.insertAdjacentHTML('beforeend', createMarkup(hits));
+    // const refreshPage = new SimpleLightbox('.gallery a', {
+    //   captionsData: 'alt',
+    //   captionDelay: 250,
+    // });
+    refreshPage.refresh();
+    scrollBy()
+} catch (error) {
+  onFetchError(error);
+}finally{
+  
+  loadMoreBtn.disabled = false;
+  if(page === maxPage){
+    loadMoreBtn.classList.add(hiddenClass);
+    loadMoreBtn.removeEventListener("click", handleLoadMore)
+  iziToast.error({
+    title: 'Error',
+    message:
+      "We're sorry, but you've reached the end of search results.",
+  });
+  
+  }
+}
+}
+
+function onFetchError(error){
+  iziToast.error({
+    title: 'Error',
+    message:
+      'OOps... Plese try again',
+  });
+}
+
+function scrollBy() {
+  window.scrollBy({
+    top: 700,
+    behavior: 'smooth',
+  });
 }
